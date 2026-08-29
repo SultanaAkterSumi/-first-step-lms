5206f249bf7e49e229d34259dff8ee9127920fe7
SultanaAkterSumi <sultanaakter4950@gmail.com>
2026-08-29 16:18:14 +0600

    fix: update strapi-server to v5 syntax

--- backend/src/extensions/users-permissions/strapi-server.js
+++ backend/src/extensions/users-permissions/strapi-server.js
@@ -6,18 +6,19 @@ module.exports = (plugin) => {
     await originalMe(ctx);
 
     if (ctx.body && ctx.body.id) {
-      const user = await strapi.entityService.findOne(
-        "plugin::users-permissions.user",
-        ctx.body.id,
-        { populate: ["role"] },
-      );
+      const user = await strapi
+        .documents("plugin::users-permissions.user")
+        .findOne({
+          documentId: ctx.body.documentId,
+          populate: ["role"],
+        });
 
-      // Password and sensitive data hide
-      delete user.password;
-      delete user.resetPasswordToken;
-      delete user.confirmationToken;
-
-      ctx.body = user;
+      if (user) {
+        delete user.password;
+        delete user.resetPasswordToken;
+        delete user.confirmationToken;
+        ctx.body = user;
+      }
     }
   };
 
