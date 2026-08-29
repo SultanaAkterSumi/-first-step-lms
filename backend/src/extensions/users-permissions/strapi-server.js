0c38e8b2480751e4c0a50026d240e565d6f4a711
SultanaAkterSumi <sultanaakter4950@gmail.com>
2026-08-29 16:26:32 +0600

    fix: disable custom me endpoint

--- backend/src/extensions/users-permissions/strapi-server.js
+++ backend/src/extensions/users-permissions/strapi-server.js
@@ -1,31 +1,4 @@
 // @ts-nocheck
 module.exports = (plugin) => {
-  const originalMe = plugin.controllers.user.me;
-
-  plugin.controllers.user.me = async (ctx) => {
-    await originalMe(ctx);
-
-    if (ctx.body && ctx.body.id) {
-      try {
-        const users = await strapi.db
-          .query("plugin::users-permissions.user")
-          .findMany({
-            where: { id: ctx.body.id },
-            populate: ["role"],
-          });
-
-        if (users && users[0]) {
-          const user = users[0];
-          delete user.password;
-          delete user.resetPasswordToken;
-          delete user.confirmationToken;
-          ctx.body = user;
-        }
-      } catch (err) {
-        console.error("Error populating role:", err);
-      }
-    }
-  };
-
   return plugin;
 };
